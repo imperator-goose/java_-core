4e3f8c2045d3040c326fb5c8667e1ecf3041888c
imperator-goose <glookeemaska133@gmail.com>
2023-05-24 16:31:41 +0300

    first commit

---
+++ src/chapter2/Main.java
@@ -0,0 +1,10 @@
+package chapter2;
+
+class Main{
+    public static void main(String[] args) {
+        int x, y;
+        x = 10;
+        y = 20;
+        if(x < y) System.out.println("x < y");
+    }
+}
