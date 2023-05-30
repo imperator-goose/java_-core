5f557130fad8c5b8268c7b5ce513f883d9bc02f5
imperator-goose <glookeemaska133@gmail.com>
2023-05-30 14:14:36 +0300

    if_else_ifv2

--- src/chapter5/if_else_if.java
+++ src/chapter5/if_else_if.java
@@ -2,14 +2,12 @@ package chapter5;
 
 public class if_else_if {
     public static void main(String[] args) {
-
-
         int a = 1;
         int i = 2;
-        if (a > i) {
-            System.out.println("авава");
- } else if (a < i) {
-            System.out.println("а");
+        if (a > i){
+            System.out.println("dfd");
+        } else if (a < i) {
+            System.out.println("df");
         }
     }
 }
