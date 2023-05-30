e4e83e2246590c165854af291ce348894b5b152b
imperator-goose <glookeemaska133@gmail.com>
2023-05-30 14:08:03 +0300

    if_else_if

---
+++ src/chapter5/if_else_if.java
@@ -0,0 +1,15 @@
+package chapter5;
+
+public class if_else_if {
+    public static void main(String[] args) {
+
+
+        int a = 1;
+        int i = 2;
+        if (a > i) {
+            System.out.println("авава");
+ } else if (a < i) {
+            System.out.println("а");
+        }
+    }
+}
